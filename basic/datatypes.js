6d67e89b840729cacc08c66190ac51da7911a94a
Diwakar Singh <65461299+Diw14@users.noreply.github.com>
2023-08-16 23:23:47 +0530

    learn

---
+++ basic/datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // to treat all JS code as new version of js
+
+/*
+alert(3+3) // will not work in nodejs, but will work in browser
+*/
+
+// ===== PRIMITIVE =======
+// number => 2 to power 53
+// bigint -> to store values > 2^53
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => not initialised
+// symbol => unique
+
+// ===== DERIVED =====
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
